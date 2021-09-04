248196997eb747c5d062587bf622fb7ea4495b57
philip howley <howley.phil@gmail.com>
2021-09-04 11:19:02 -0400

    Add custom js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,38 @@
+// let myName = 'Tom';
+// let myName = 'phil';
+// console.log(myName);
+const age = 0;
+const isSoccerFan = true;
+let score = null;
+const colorsArr = ['red', 'blue', 'green'];
+const person = {
+  firstName: 'Phil',
+  age: 22,
+  likesSoccer: true
+}
+
+if (age) {
+  console.log('you are 18')
+} else {
+  console.log('you are not 18');
+}
+
+// console.log(score);
+// console.log(typeof myName);
+// console.log(typeof age);
+// console.log(typeof isSoccerFan);
+
+// if (age >= 21) {
+//  console.log('you can drink');
+// } else {
+//  console.log("you can't drink");
+// }
+
+
+
+
+
+
 // const myBtnEl = document.querySelector('.my-button');
 // const myTxtEl = document.querySelector('.my-text');
 
@@ -6,6 +41,6 @@
 // }
 
 // myBtnEl.addEventListener('click', swapSomeText);
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip();
-});
+// $(function () {
+//   $('[data-toggle="tooltip"]').tooltip();
+// });
