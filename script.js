13e39f114b2315fa949005574481e40fac6e97a0
philip howley <howley.phil@gmail.com>
2021-09-04 09:27:08 -0400

    Review data attibutes

---
+++ script.js
@@ -0,0 +1,8 @@
+const myBtnEl = document.querySelector('.my-button');
+const myTxtEl = document.querySelector('.my-text');
+
+function swapSomeText() {
+  myTxtEl.textContent = myBtnEl.getAttribute('data-whatever');
+}
+
+myBtnEl.addEventListener('click', swapSomeText);
